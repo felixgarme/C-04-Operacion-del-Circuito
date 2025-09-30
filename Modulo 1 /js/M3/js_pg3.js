9050ce88224a5ff54be40f93a92b41c20cc2e91c
Felix Garcia Mestas <mzpro123@gmail.com>
2025-09-29 23:14:47 -0500

    1.4.5

--- Modulo 1 /js/M3/js_pg3.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const sections = document.querySelectorAll('.transfer-material, .molienda-sag');
-
-  sections.forEach(section => {
-    section.addEventListener('click', () => {
-      section.classList.toggle('active');
-    });
-  });
-});
